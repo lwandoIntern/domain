c82e9b96bd663c0c53859709213083f4f8c620cd
lwandoIntern <ncebagoniwe@gmail.com>
2019-05-13 14:36:34 +0200

    update repository

---
+++ src/main/java/za/ac/cput/domain/controller/IndexController.java
@@ -0,0 +1,14 @@
+package za.ac.cput.domain.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    @ResponseBody
+    public String getDomain(){
+        return "Domain";
+    }
+}
