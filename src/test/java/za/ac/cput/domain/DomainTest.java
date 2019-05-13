7d34dd1f7a72cd0df9d971b725f0baca67ee94fd
lwandoIntern <ncebagoniwe@gmail.com>
2019-05-13 14:15:35 +0200

    name change

--- src/test/java/za/ac/cput/domain/DomainTest.java
+++ src/test/java/za/ac/cput/domain/DomainTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple Domain.
  */
-public class AppTest 
+public class DomainTest
 {
     /**
      * Rigorous Test :-)
