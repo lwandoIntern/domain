26eb5cb27be7661bdea39a1bca6c50d3a58554f1
lwandoIntern <ncebagoniwe@gmail.com>
2019-05-13 14:49:41 +0200

    update controller

--- src/main/java/za/ac/cput/domain/controller/author_controller/AuthorController.java
+++ src/main/java/za/ac/cput/domain/controller/author_controller/AuthorController.java
@@ -1,4 +1,4 @@
-package za.ac.cput.domain.controller;
+package za.ac.cput.domain.controller.author_controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -7,12 +7,12 @@ import za.ac.cput.domain.domain.authors.Author;
 import za.ac.cput.domain.services.author.impl.AuthorServiceImpl;
 
 @RestController
-@RequestMapping("/domain/controller")
+@RequestMapping("/author")
 public class AuthorController {
     @Autowired
     private AuthorServiceImpl service;
 
-    @RequestMapping(value = "/{name}/{surname}/{cell}/{tell}/{zip}/{surbub}/{city}/{prov}/{numBooks}",method = RequestMethod.GET)
+    @RequestMapping("/create")
     @ResponseBody
     public Author create(@PathVariable String name, @PathVariable String surname, @PathVariable String cell, @PathVariable String tell,
                          @PathVariable int zip, @PathVariable String surbub, @PathVariable String city, @PathVariable String prov,
