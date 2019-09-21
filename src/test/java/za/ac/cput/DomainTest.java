8bd7370c594e3006a563dbd3940d7fd52b75465a
lwandoIntern <45486492+lwandoIntern@users.noreply.github.com>
2019-09-21 11:45:14 +0200

    Add files via upload

---
+++ src/test/java/za/ac/cput/DomainTest.java
@@ -0,0 +1,20 @@
+package za.ac.cput;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class DomainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
