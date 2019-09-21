b25c6a05032f6160bc20908b458ad2c812ece5cd
lwandoIntern <45486492+lwandoIntern@users.noreply.github.com>
2019-09-21 11:11:51 +0200

    SECURITY!!!

---
+++ src/main/java/za/ac/cput/security/DomainSecurity.java
@@ -0,0 +1,47 @@
+package za.ac.cput.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DomainSecurity extends WebSecurityConfigurerAdapter {
+    private static final String USER_ROLE = "USER";
+    private static final String ADIMN_ROLE = "ADMIN";
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(encoder().encode("staff"))
+                .roles(USER_ROLE)
+                .and()
+                .withUser(encoder().encode("admin"))
+                .password("admin")
+                .roles(USER_ROLE,ADIMN_ROLE);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/domain/**").hasRole(USER_ROLE)
+                .and()
+                .csrf()
+                .disable()
+                .formLogin()
+                .disable();
+
+    }
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
