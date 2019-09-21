75625b80e33d53f45f709c72b03e0dae9885480f
lwandoIntern <ncebagoniwe@gmail.com>
2019-09-21 12:42:16 +0200

    test issues fixed

---
+++ src/test/java/za/ac/cput/factory/author/AuthorFactoryTest.java
@@ -0,0 +1,17 @@
+package za.ac.cput.factory.author;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthorFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createAuthor() {
+    }
+}
